1ecd39c195af73919213ba3ec13f3fad6e62044b
ta9ta9008da@gmail.com <owadataku@oowadatakunoAir.lan>
2022-02-18 19:39:51 +0900

    ラムダ式の問題演習を追加

--- src/rensyu_mondai/Main.java
+++ src/rensyu_mondai/Main.java
@@ -3,10 +3,7 @@ import static rensyu_mondai.AccountType.*;
 import static rensyu_mondai.KeyType.*;
 
 import java.util.*;
-import java.util.function.BiFunction;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Supplier;
+import java.util.function.*;
 import java.text.SimpleDateFormat;
 public class Main {	
 
@@ -16,21 +13,27 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		//lenメソッドの処理ロジックを、変数funcに代入する
-		Function<String, Integer> func = Main::len;
+		Function<String , Integer> func = (String s) -> {return s.length();};
+		int n = func.apply("Java");
+		System.out.println("文字列「Java」は"+ n + "文字です");
 		
-		//変数funcに格納されている処理ロジックを、引数"Java"で実行する
-		int a = func.apply("Java");
-		System.out.println("文字列「java」は"+ a + "文字です");
 		
-		Consumer<String> func2 = System.out::println;
-		func2.accept("Hello , World");
 		
-		Supplier<String> func3 = System::lineSeparator;
-		System.out.println("改行します" + func3.get() );
-		
-		BiFunction<String , String , String> func4 = System::getProperty;
-		System.out.println(func4.apply("java.version", "不明"));
+//		//lenメソッドの処理ロジックを、変数funcに代入する
+//		Function<String, Integer> func = Main::len;
+//		
+//		//変数funcに格納されている処理ロジックを、引数"Java"で実行する
+//		int a = func.apply("Java");
+//		System.out.println("文字列「java」は"+ a + "文字です");
+//		
+//		Consumer<String> func2 = System.out::println;
+//		func2.accept("Hello , World");
+//		
+//		Supplier<String> func3 = System::lineSeparator;
+//		System.out.println("改行します" + func3.get() );
+//		
+//		BiFunction<String , String , String> func4 = System::getProperty;
+//		System.out.println(func4.apply("java.version", "不明"));
 		
 	}
 	
