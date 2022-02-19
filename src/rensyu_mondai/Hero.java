8325d90ae0109798e5b1fe5462c94ff6c29bf224
ta9ta9008da@gmail.com <owadataku@oowadatakunoAir.lan>
2022-02-19 11:04:07 +0900

    ラムダの練習追加

--- src/rensyu_mondai/Hero.java
+++ src/rensyu_mondai/Hero.java
@@ -8,6 +8,14 @@ public class Hero implements Cloneable {
 	private int hp,mp;
 	private Sword sword;
 	
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+	
+	public int getHp() {
+		return this.hp ;
+	}
+	
 	
 	public void setSword(Sword sword) {
 		this.sword= sword;
--- src/rensyu_mondai/Main.java
+++ src/rensyu_mondai/Main.java
@@ -6,17 +6,66 @@ import java.util.*;
 import java.util.function.*;
 import java.text.SimpleDateFormat;
 public class Main {	
-
-	public static Integer len (String s) {
-		return s.length();
-	}
 	
 	public static void main(String[] args) {
 		
-		Function<String , Integer> func = (String s) -> {return s.length();};
-		int n = func.apply("Java");
-		System.out.println("文字列「Java」は"+ n + "文字です");
 		
+		List<Hero> heroes = new ArrayList<>();
+		
+		boolean anyoneKnockedOut =  heroes.stream().anyMatch(h -> h.getHp() == 0);
+		
+		
+		List<Account> list = new ArrayList<>();
+		
+		Collections.sort(list , (x,y)->(x.getZandaka() - y.getZandaka()));
+		
+		
+		IntToDoubleFunction func = (int x) -> {
+			return  x * x * 3.14;
+		};
+		
+		System.out.println(func.applyAsDouble(1));
+		
+		IntToDoubleFunction func2 = (x) -> {
+			return x * x * 3.14;
+		};
+		
+		IntToDoubleFunction func3 =   x -> {
+			return x * x * 3.14;
+		};
+		
+		IntToDoubleFunction func4 =   x ->  x * x * 3.14;
+		
+		double b = 3.14;
+		IntToDoubleFunction func5 = (int x) -> {
+			return x * x * b ;
+		};
+		
+		
+		//例１　勇者インスタンスを受け取り、そのHPを返す
+		// (Hero h) -> {return h.getHp();}
+		
+		//例２　何も受け取らず、現在日時を返す
+		//() -> {return new java.util.Date();}
+		
+		//例3　long配列を受け取り、そのコピーを作り、内容を並び替えて返す
+//		(long[] org) -> {
+//			long[] cpy = java.util.Arrays.copyOf(org , org.length);
+//			java.util.Arrays.sort(cpy);
+//			return cpy;
+//			
+//		}
+		
+		//例４　関数オブジェクトを受け取り、２回呼び出した合計を返す
+//		(IntBinaryOperator func , int a , int b) -> {
+//			int result = func.applyAsInt(a, b) + func.applyAsInt(a,b);
+//			return result;
+//		}
+		
+//		Function<String , Integer> func = (String s) -> {return s.length();};
+//		int n = func.apply("Java");
+//		System.out.println("文字列「Java」は"+ n + "文字です");
+//		
 		
 		
 //		//lenメソッドの処理ロジックを、変数funcに代入する
