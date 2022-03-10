49f77eae092dd6f28fa902c57b9ade2469a218de
Taku-Owada-Private <ta9ta9008da@gmail.com>
2022-03-10 11:51:28 +0900

    最新化

--- src/rensyu_mondai/Main.java
+++ src/rensyu_mondai/Main.java
@@ -40,10 +40,14 @@ public class Main {
 		
 		
 		
-		System.out.println("アサートにわざと失敗します");
-		assert 1 == 0;
-		System.out.println("正常終了します");
 		
+		System.out.println("Hello World!!");
+		
+//		
+//		System.out.println("アサートにわざと失敗します");
+//		assert 1 == 0;
+//		System.out.println("正常終了します");
+//		
 //		
 //		System.out.println("1円以上のアイテム一覧を表示します");
 //		ArrayList<Item> items = ItemsDAO.findByMinimumPrice(1);
